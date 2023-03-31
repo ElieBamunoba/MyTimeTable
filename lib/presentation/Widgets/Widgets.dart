b4302e6761a3660c765e659f109309586c01fc80
dev30k <develop_kk@outlook.com>
2023-04-01 01:14:49 +0300

    color pallete

---
+++ lib/presentation/Widgets/Widgets.dart
@@ -0,0 +1,189 @@
+import 'package:exam_time_table/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0xfffbfbfb),
+                        Color(0xfff7f7f7),
+
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+
+                    ),
+
+                  ),
+    child: Column(crossAxisAlignment:CrossAxisAlignment.start,
+      children: [
+        appBar(),
+        titleBar(),
+      ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  // widget to display daystar university at home page
+  Widget appTitle(){
+    return Container(
+      padding: const EdgeInsets.all(6),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(18)),
+
+        border: Border.all(
+          color: AppColors.darkBlue,
+          width: 1,
+        ),
+        boxShadow: <BoxShadow>[
+          const BoxShadow(
+            color: Color(0xfffbf2ef),
+            blurRadius: 10,
+            spreadRadius: 5,
+            offset: Offset(5, 5),
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          const Icon(Icons.location_pin,color: AppColors.darkBlue,size: 18,),
+          Container(
+            child: const TitleText(
+              text: 'My TimeTable',
+              fontWeight: FontWeight.w700,
+              fontSize: 15,
+            ),
+          )
+        ],
+      ),
+        );
+  }
+  Widget appBar(){
+    return Container(
+      padding:EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          appTitle(),
+          Container(height: 40,width: 40,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AppColors.darkBlue,
+              style: BorderStyle.none,
+
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(13)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color(0xfff8f8f8),
+                  blurRadius: 5,
+                  spreadRadius: 10,
+                  offset: Offset(5, 5)),
+            ],
+          ),
+          child: Icon(
+            Icons.notifications,
+            color: Colors.black,
+            size: 30,
+          ),).ripple(() {},borderRadius: BorderRadius.all(Radius.circular((13))))
+
+        ],
+      ),
+
+    );
+  }
+  Widget titleBar(){
+    DateTime date = new DateTime.now();
+    return Container(
+      margin: EdgeInsets.all(12),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TitleText(text: '${DateFormat.yMMMMEEEEd().format(date)}',fontSize: 12,fontWeight: FontWeight.w200,),
+              TitleText(text: 'Welcome Student!',fontSize: 19,fontWeight: FontWeight.w700,)
+            ],
+          ),
+          ],
+      ),
+    );
+
+
+  }
+
+
+}
+// class to customize text widget
+class TitleText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+  final FontWeight fontWeight;
+
+  const TitleText(
+      {Key? key,
+      required this.text,
+      this.fontSize = 18,
+      this.color = AppColors.textColor,
+      this.fontWeight = FontWeight.w800})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: GoogleFonts.mulish(
+            fontSize: fontSize, fontWeight: fontWeight, color: color));
+  }
+}
+
+
+
+extension OnPressed on Widget {
+  Widget ripple(Function onPressed,
+          {BorderRadiusGeometry borderRadius =
+              const BorderRadius.all(Radius.circular(5))}) =>
+      Stack(
+        children: <Widget>[
+          this,
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: TextButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(borderRadius: borderRadius),
+                )),
+                onPressed: () {
+                  if (onPressed != null) {
+                    onPressed();
+                  }
+                },
+                child: Container()),
+          )
+        ],
+      );
+}
