52eff279f4fe7df777489e4055981945da791d4e
ElieBamunoba <eliebamunoba@gmail.com>
2023-04-01 18:56:56 +0300

    ui update

--- lib/presentation/widgets/unit_card.dart
+++ lib/presentation/widgets/unit_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '/constants/colors.dart';
 
@@ -16,63 +17,96 @@ class UnitCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Convert string to DateTime object
+    final date = (examDay.trim()).split(' ');
+    String datePart = date[1]; // "17/04/23"
+
     return Card(
-      elevation: 5,
+      elevation: 0,
+      color: AppColors.ligthBlue,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-        side: const BorderSide(color: AppColors.darkBlue),
+        borderRadius: BorderRadius.circular(10),
+        side: const BorderSide(color: AppColors.ligthBlue),
       ),
       child: Container(
-        height: MediaQuery.of(context).size.height * .1,
+        height: 100,
         padding: const EdgeInsets.all(10),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                unitCode,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Text(
-                    'Date: ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(examDay),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  const Text(
-                    'Venue: ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(examVenue),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Text(
-                    'Time: ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(examTime),
-                ],
-              ),
-            ],
-          ),
-        ]),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  unitCode,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textColor,
+                      fontSize: 17),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    const Text(
+                      'Date: ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textDarkGrey),
+                    ),
+                    Text(
+                      DateFormat('EEE dd/MM/yy')
+                          .format(DateFormat('dd/MM/yy').parse(datePart)),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.darkBlue),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'Venue: ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textDarkGrey),
+                    ),
+                    Text(
+                      examVenue,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.darkBlue),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    const Text(
+                      'Time: ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textDarkGrey),
+                    ),
+                    Text(
+                      examTime,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.darkBlue),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
