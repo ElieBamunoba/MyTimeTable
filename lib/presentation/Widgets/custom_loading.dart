857793f0ce3885274f1e0b129e3fb8d940fb8eaf
ElieBamunoba <eliebamunoba@gmail.com>
2023-04-01 16:44:16 +0300

    scroll behavior

---
+++ lib/presentation/Widgets/custom_loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+
+class CustomLoading extends StatelessWidget {
+  const CustomLoading({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .6,
+      child: Column(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/loading2.gif',
+              height: 250,
+            ),
+          ),
+          Text('Making the magic happen! 😎',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontSize: 18, color: AppColors.darkBlue)),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/landing_screen.dart
+++ lib/presentation/landing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:exam_time_table/presentation/widgets/scroll_behavior.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,6 +7,7 @@ import '../constants/colors.dart';
 import './widgets/search_box.dart';
 import './widgets/title_text.dart';
 import 'Widgets/up_coming_unit_card.dart';
+import 'widgets/custom_loading.dart';
 import 'widgets/unit_card.dart';
 
 class LandingScreen extends StatelessWidget {
@@ -28,103 +30,87 @@ class LandingScreen extends StatelessWidget {
             )
           ],
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const TitleText(
-                    text: 'Welcome Freind !',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700),
-                const SizedBox(height: 10),
-                SearchBox(),
-                const SizedBox(height: 10),
-                BlocBuilder<UnitBloc, UnitState>(
-                  builder: (context, state) {
-                    if (state is UnitLoading) {
-                      return Builder(builder: (context) {
-                        return SizedBox(
-                          height: MediaQuery.of(context).size.height * .6,
+        body: ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height - 60,
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TitleText(
+                      text: 'Welcome Freind !',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                  const SizedBox(height: 10),
+                  SearchBox(),
+                  const SizedBox(height: 10),
+                  BlocBuilder<UnitBloc, UnitState>(
+                    builder: (context, state) {
+                      if (state is UnitLoading) {
+                        return Builder(builder: (context) {
+                          return const CustomLoading();
+                        });
+                      } else if (state is UnitLoaded) {
+                        return Expanded(
                           child: Column(
                             children: [
-                              Center(
-                                child: Image.asset(
-                                  'assets/loading2.gif',
-                                  height: 250,
+                              UpcomingUnitCard(
+                                unitCode:
+                                    state.unitsList[0].courseCode.toString(),
+                                examDay: state.unitsList[0].date.toString(),
+                                examTime: state.unitsList[0].time.toString(),
+                                examVenue: state.unitsList[0].venue.toString(),
+                              ),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: state.unitsList.length,
+                                  itemBuilder: (context, index) => UnitCard(
+                                    unitCode: state.unitsList[index].courseCode
+                                        .toString(),
+                                    examDay:
+                                        state.unitsList[index].date.toString(),
+                                    examTime:
+                                        state.unitsList[index].time.toString(),
+                                    examVenue:
+                                        state.unitsList[index].venue.toString(),
+                                  ),
                                 ),
                               ),
-                              Text('Making the magic happen! 😎',
+                            ],
+                          ),
+                        );
+                      } else if (state is UnitInitial) {
+                        return SizedBox(
+                          height: MediaQuery.of(context).size.height * .5,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Enter your unit code to get a simplified timetable of your exams',
+                                  textAlign: TextAlign.center,
                                   style: Theme.of(context)
                                       .textTheme
                                       .headlineSmall!
                                       .copyWith(
-                                          fontSize: 18,
-                                          color: AppColors.darkBlue)),
-                            ],
+                                          color: Colors.black, fontSize: 15),
+                                ),
+                              ],
+                            ),
                           ),
                         );
-                      });
-                    } else if (state is UnitLoaded) {
-                      return Expanded(
-                        child: Column(
-                          children: [
-                            UpcomingUnitCard(
-                              unitCode:
-                                  state.unitsList[0].courseCode.toString(),
-                              examDay: state.unitsList[0].date.toString(),
-                              examTime: state.unitsList[0].time.toString(),
-                              examVenue: state.unitsList[0].venue.toString(),
-                            ),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: state.unitsList.length,
-                                itemBuilder: (context, index) => UnitCard(
-                                  unitCode: state.unitsList[index].courseCode
-                                      .toString(),
-                                  examDay:
-                                      state.unitsList[index].date.toString(),
-                                  examTime:
-                                      state.unitsList[index].time.toString(),
-                                  examVenue:
-                                      state.unitsList[index].venue.toString(),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    } else if (state is UnitInitial) {
-                      return SizedBox(
-                        height: MediaQuery.of(context).size.height * .5,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Enter your unit code to get a simplified timetable of your exams',
-                                textAlign: TextAlign.center,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headlineSmall!
-                                    .copyWith(
-                                        color: Colors.black, fontSize: 15),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    } else {
-                      return const Center(
-                        child: Text('Something went wrong'),
-                      );
-                    }
-                  },
-                ),
-              ],
+                      } else {
+                        return const Center(
+                          child: Text('Something went wrong'),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ));
