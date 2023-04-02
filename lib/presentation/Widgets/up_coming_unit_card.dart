b405f54f2cc7385546ab23266773c755e4416711
johnmuinde <jhnmuinde@gmail.com>
2023-04-02 21:03:41 +0300

    countdown logic

--- lib/presentation/Widgets/up_coming_unit_card.dart
+++ lib/presentation/Widgets/up_coming_unit_card.dart
@@ -1,16 +1,98 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../bloc/saved_units/saved_units_bloc.dart';
 import '../../models/unit_model.dart';
 import '/constants/colors.dart';
 
-class UpcomingUnitCard extends StatelessWidget {
+class UpcomingUnitCard extends StatefulWidget {
   const UpcomingUnitCard({
-    super.key,
+    Key? key,
     required this.unit,
-  });
+  }) : super(key: key);
   final UnitModel unit;
+
+  @override
+  State<UpcomingUnitCard> createState() => _UpcomingUnitCardState();
+}
+
+class _UpcomingUnitCardState extends State<UpcomingUnitCard> {
+  String message = "";
+
+  String formatTime(int time, String singular, String plural) {
+    if (time == 1) {
+      return "${NumberFormat().format(time)} $singular";
+    } else {
+      return "${NumberFormat().format(time)} $plural";
+    }
+  }
+
+  void startTimer() {
+    final date = widget.unit?.date ?? '0000 00/00/00';
+    final time = widget.unit?.time ?? '00:00AM-00:00AM';
+    DateTime startTime = DateFormat('dd/MM/yy hh:mma')
+        .parse('${date.split(" ")[1]} ${time.split('-')[0]}');
+    // Calculate the difference between the start time and the current time
+    Duration difference = startTime.difference(DateTime.now());
+
+    // If the start time is in the past, set the duration to 0
+    if (difference.isNegative) {
+      difference = Duration(seconds: 0);
+    }
+
+    // Create a stream that emits a value every second
+    Stream<int> stream = Stream.periodic(Duration(seconds: 1), (int count) {
+      setState(() {});
+      message = "";
+      return count;
+    });
+
+    // Declare the subscription variable
+    StreamSubscription? subscription;
+
+    // Listen to the stream and execute some code every time a value is emitted
+    subscription = stream.listen((int count) {
+      Duration remaining = difference - Duration(seconds: count);
+
+      if (remaining.isNegative) {
+        // If the remaining time is negative, cancel the subscription
+        subscription!.cancel();
+      } else {
+        int days = remaining.inDays;
+        int hours = remaining.inHours % 24;
+        int minutes = remaining.inMinutes % 60;
+        int seconds = remaining.inSeconds % 60;
+
+        if (remaining.inDays >= 1) {
+          message += formatTime(days, "day", "days");
+          if (hours > 0) {
+            message +=
+                " ${formatTime(hours, "hour", "hours")} ${formatTime(minutes, "minute", "minutes")}";
+          }
+        } else if (remaining.inHours >= 1) {
+          message += formatTime(hours, "hour", "hours");
+          if (minutes > 0) {
+            message += " ${formatTime(minutes, "minute", "minutes")} ";
+          }
+        } else {
+          message += formatTime(minutes, "minute", "minutes");
+          if (seconds > 0) {
+            message += " ${formatTime(seconds, "second", "seconds")}";
+          }
+        }
+      }
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("heyyyyyyyyyyyyyyyyyyyyyy");
+    startTimer();
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +124,8 @@ class UpcomingUnitCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(unit.courseCode.toString()),
-                Text(unit.time.toString()),
+                Text(widget.unit.courseCode.toString()),
+                Text(widget.unit.time.toString()),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -55,7 +137,7 @@ class UpcomingUnitCard extends StatelessWidget {
                           size: 16,
                         ),
                         const SizedBox(width: 10),
-                        Text(unit.date.toString()),
+                        Text(widget.unit.date.toString()),
                       ],
                     ),
                     Row(
@@ -66,12 +148,12 @@ class UpcomingUnitCard extends StatelessWidget {
                           size: 16,
                         ),
                         const SizedBox(width: 5),
-                        Text(unit.venue.toString()),
+                        Text(widget.unit.venue.toString()),
                       ],
                     ),
                   ],
                 ),
-                const Text("Countdown: 13hrs"),
+                Text("$message remaining"),
               ],
             ),
           ),
@@ -89,7 +171,9 @@ class UpcomingUnitCard extends StatelessWidget {
               icon: const Icon(Icons.delete_forever,
                   color: Colors.white, size: 25),
               onPressed: () {
-                context.read<SavedUnitsBloc>().add(DeleteUnit(unit: unit));
+                context
+                    .read<SavedUnitsBloc>()
+                    .add(DeleteUnit(unit: widget.unit));
                 context.read<SavedUnitsBloc>().add(LoadSavedUnits());
               },
             ),
